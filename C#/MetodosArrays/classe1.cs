1c362ae001cda9c5df829584f18a58526649aa11
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-02-03 22:44:30 -0300

    Tipos de classes

---
+++ C#/MetodosArrays/classe1.cs
@@ -0,0 +1,10 @@
+using System;    
+
+class index {
+// public, Publica sem restrição de visualizção 
+// abstract : Classe-base para outras classes, não podem ser usadas para gerar objetos
+// sealed classe não pode ser herdada
+// static classe não permite a instaciação de objetos e seus membros. 
+
+
+}
