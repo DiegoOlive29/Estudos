9d8146800a4fb4c16a741b52736be8c4fe3f4a0f
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-02-04 23:31:45 -0300

    Metodo construtor

--- C#/MetodosArrays/classe1.cs
+++ C#/MetodosArrays/classe1.cs
@@ -18,22 +18,30 @@ using System;
     // static: o metodo pdoe ser chamado mesmo sem a instaciação de um objeto.
 
 public class Player {
-    public int vida=100;
-    public bool vivo = true;
+    public int vida;
+    public bool vivo;
+    public string nome;
+
+    //metodo contrutor, precisa ter o nome da classe.
+    public Player(string n){
+        vida = 100;
+        vivo = true;
+        nome = n;
+    }
 
 
 }
 
 class Index{
     static void Main(){
-        Player p1 = new Player();
-        Player p2 = new Player();
-        Player p3 = new Player();
+        Player p1 = new Player("Diego");
+        Player p2 = new Player("Luan");
+        Player p3 = new Player("Pedro");
 
-        p1.vida =50;
-        
 
-        Console.WriteLine("Vida do jogador 1: {0}", p1.vida);
+        Console.WriteLine("Vida do jogador 1: {0}", p1.nome);
+        Console.WriteLine("Vida do jogador 1: {0}", p2.nome);
+        Console.WriteLine("Vida do jogador 1: {0}", p3.nome);
 
     }
 }
